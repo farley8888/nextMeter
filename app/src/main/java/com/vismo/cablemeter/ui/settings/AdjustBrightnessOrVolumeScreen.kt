631a185746bb3988810564b1175c947562e0f649
dasprasky <prasenjitdaswk@gmail.com>
2024-10-23 16:23:43 +0800

    ui enhancements

--- app/src/main/java/com/vismo/cablemeter/ui/settings/AdjustBrightnessOrVolumeScreen.kt
+++ app/src/main/java/com/vismo/cablemeter/ui/settings/AdjustBrightnessOrVolumeScreen.kt
@@ -2,11 +2,18 @@ package com.vismo.cablemeter.ui.settings
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.VolumeDown
+import androidx.compose.material.icons.automirrored.filled.VolumeUp
+import androidx.compose.material.icons.filled.BrightnessHigh
+import androidx.compose.material.icons.filled.BrightnessLow
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,21 +37,57 @@ fun AdjustBrightnessOrVolumeScreen(
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Brightness")
-        Slider(
-            value = brightness,
-            onValueChange = { viewModel.updateBrightness(it) },
-            valueRange = 0f..1f,
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Text(text = "Volume")
-        Slider(
+        SettingSliderRow(
             value = volume,
             onValueChange = { viewModel.updateVolume(it) },
-            valueRange = 0f..1f,
-            modifier = Modifier.fillMaxWidth()
+            iconStart = { Icon(imageVector = Icons.AutoMirrored.Filled.VolumeDown, contentDescription = "Volume Down Icon") },
+            label = "Volume",
+            iconEnd = { Icon(imageVector = Icons.AutoMirrored.Filled.VolumeUp, contentDescription = "Volume Up Icon") }
+        )
+        Spacer(modifier = Modifier.height(28.dp))
+        SettingSliderRow(
+            value = brightness,
+            onValueChange = { viewModel.updateBrightness(it) },
+            iconStart = { Icon(imageVector = Icons.Default.BrightnessLow, contentDescription = "Brightness Low Icon") },
+            label = "Brightness",
+            iconEnd = { Icon(imageVector = Icons.Default.BrightnessHigh, contentDescription = "Brightness High Icon") }
+        )
+    }
+}
+
+@Composable
+fun SettingSliderRow(
+    value: Float,
+    onValueChange: (Float) -> Unit,
+    iconStart: @Composable () -> Unit,
+    iconEnd: @Composable () -> Unit,
+    label: String
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            iconStart()
+            Slider(
+                value = value,
+                onValueChange = onValueChange,
+                valueRange = 0f..1f,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 16.dp)
+            )
+            iconEnd()
+        }
+        Text(
+            text = label,
+            modifier = Modifier.padding(start = 16.dp, top = 4.dp)
         )
     }
 }
