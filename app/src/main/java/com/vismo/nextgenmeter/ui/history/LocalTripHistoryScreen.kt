7a72bad8ef4294e3aa1fdd6fcbf13c9a8305b3bb
Roy Chan <roy.chan@vis-mobility.com>
2024-11-24 18:45:41 +0800

    update meterop and localtriphistory screen

--- app/src/main/java/com/vismo/nextgenmeter/ui/history/LocalTripHistoryScreen.kt
+++ app/src/main/java/com/vismo/nextgenmeter/ui/history/LocalTripHistoryScreen.kt
@@ -1,16 +1,26 @@
 package com.vismo.nextgenmeter.ui.history
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +30,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -27,7 +38,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.KeyEventType.Companion.KeyDown
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.input.key.type
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.vismo.nextgenmeter.model.TripData
@@ -65,12 +79,36 @@ fun LocalTripHistoryScreen(viewModel: LocalTripHistoryViewModel) {
             TripHeaderRow()
         }
         // Add trip items
-        items(trips) { trip ->
-            TripItemRow(
-                trip = trip,
-                isSelected = trip.tripId == selectedTripId,
-                onClick = { selectedTripId = trip.tripId }
-            )
+        // if trips is empty, show a message
+        if (trips.isEmpty()) {
+            item {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .border(1.dp, Color.Gray, RoundedCornerShape(8.dp))
+                        .padding(8.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "暫無行程",
+                        fontSize = 24.sp,
+                        color = Color.Gray
+                    )
+                }
+            }
+        } else {
+            items(trips) { trip ->
+                TripItemRow(
+                    count = trips.indexOf(trip) + 1,
+                    trip = trip,
+                    isSelected = trip.tripId == selectedTripId,
+                    onClick = { selectedTripId = trip.tripId }
+                )
+            }
+        }
+        item {
+            TripFooterRow()
         }
     }
 
@@ -88,39 +126,119 @@ fun TripHeaderRow() {
             .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.1f))
             .padding(8.dp)
     ) {
+        val fontSize = 28.sp
+        // except for the first column, all other columns have the same weight
+        Text(
+            text = "#",
+            modifier = Modifier.weight(0.5f),
+            fontSize = fontSize,
+            color = Color.White,
+            textAlign = TextAlign.Center
+        )
         val columnWeight = 1f
-        listOf("Start Time", "End Time", "Fare", "Extra", "Wait Time", "Trip Total").forEach { label ->
+        listOf("日", "始", "達", "里數", "侯時", "車費", "附加","總費").forEach { label ->
             Text(
                 text = label,
                 modifier = Modifier.weight(columnWeight),
-                fontSize = 18.sp,
-                color = gold500,
+                fontSize = fontSize,
+                color = Color.White,
                 textAlign = TextAlign.Center
             )
         }
     }
 }
 
+// This suppose to be floating footer row, but now simply a placeholder
 @Composable
-fun TripItemRow(trip: TripData, isSelected: Boolean, onClick: () -> Unit) {
+fun TripFooterRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically // Ensure vertical alignment
+    ) {
+        Row(
+            modifier = Modifier.weight(1f),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically // Add this line
+        ) {
+            Button(
+                onClick = { /* TODO */ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = gold500 // Set the button's color to gold
+                ),
+                modifier = Modifier
+                    .height(40.dp)
+                    .width(100.dp)
+                    .padding(vertical = 0.dp)
+            ) {
+                Text(
+                    text = "打印",
+                    color = Color.Black,
+                    fontSize = 14.sp
+                )
+            }
+            Text(
+                text = "現金",
+                color = Color.Red,
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .weight(1f),
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "電子支付",
+                color = pastelGreen600,
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .weight(1f),
+                textAlign = TextAlign.Center
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+    }
+}
+
+@Composable
+fun TripItemRow(count: Int, trip: TripData, isSelected: Boolean, onClick: () -> Unit) {
     val backgroundColor = if (isSelected) {
         if (trip.isDash) pastelGreen200.copy(alpha = 0.2f) else MaterialTheme.colorScheme.primary.copy(alpha = 0.2f)
     } else Color.Transparent
-    val textColor = if (trip.isDash) pastelGreen600 else MaterialTheme.colorScheme.onSurface
+    val defaultColor = Color.White
+    val textColor = if (trip.isDash) pastelGreen600 else Color.Red
 
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp)
+            .padding(vertical = 0.dp)
             .clickable { onClick() }
             .background(backgroundColor, RoundedCornerShape(8.dp))
-            .padding(16.dp)
+            .padding(0.dp)
     ) {
-        val columnWeight = 1f
+        val fontSize = 28.sp
         Text(
-            text = GlobalUtils.formatTimestampToTime(trip.startTime, showDate = true),
+            text = count.toString(),
+            modifier = Modifier.weight(0.5f),
+            fontSize = fontSize,
+            color = defaultColor,
+            textAlign = TextAlign.Center
+        )
+        val columnWeight = 1f
+
+        // date in dd/mm format
+        Text(
+            text = GlobalUtils.formatTimestampToDate(trip.startTime),
             modifier = Modifier.weight(columnWeight),
-            fontSize = 18.sp,
+            fontSize = fontSize,
+            color = defaultColor,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = GlobalUtils.formatTimestampToTime(trip.startTime),
+            modifier = Modifier.weight(columnWeight),
+            fontSize = fontSize,
             color = textColor,
             textAlign = TextAlign.Center
         )
@@ -128,35 +246,45 @@ fun TripItemRow(trip: TripData, isSelected: Boolean, onClick: () -> Unit) {
         Text(
             text = GlobalUtils.formatTimestampToTime(trip.pauseTime, showDate = isEndDateDifferentFromStart),
             modifier = Modifier.weight(columnWeight),
-            fontSize = 18.sp,
+            fontSize = fontSize,
             color = textColor,
+            textAlign = TextAlign.Center
+        )
+        // distance in km
+        Text(
+            text = (trip.distanceInMeter/1000).toString(),
+            modifier = Modifier.weight(columnWeight),
+            fontSize = fontSize,
+            color = defaultColor,
+            textAlign = TextAlign.Center
+        )
+        // waiting time in minutes
+        // need to convert seconds to minutes in format of MM:SS
+        Text(
+            text = GlobalUtils.formatSecondsToHHMM(trip.waitDurationInSeconds),
+            modifier = Modifier.weight(columnWeight),
+            fontSize = fontSize,
+            color = defaultColor,
             textAlign = TextAlign.Center
         )
         Text(
             text = trip.fare.toString(),
             modifier = Modifier.weight(columnWeight),
-            fontSize = 18.sp,
-            color = textColor,
+            fontSize = fontSize,
+            color = defaultColor,
             textAlign = TextAlign.Center
         )
         Text(
-            text = trip.extra.toString(),
+            text = "+"+trip.extra.toString(),
             modifier = Modifier.weight(columnWeight),
-            fontSize = 18.sp,
-            color = textColor,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = trip.waitDurationInSeconds.toString(),
-            modifier = Modifier.weight(columnWeight),
-            fontSize = 18.sp,
-            color = textColor,
+            fontSize = fontSize,
+            color = defaultColor,
             textAlign = TextAlign.Center
         )
         Text(
             text = trip.totalFare.toString(),
             modifier = Modifier.weight(columnWeight),
-            fontSize = 18.sp,
+            fontSize = fontSize,
             color = textColor,
             textAlign = TextAlign.Center
         )
