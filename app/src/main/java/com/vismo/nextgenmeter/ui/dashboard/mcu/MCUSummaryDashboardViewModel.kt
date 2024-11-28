bd7147fdc0d5d8ec18886a28914177837557641c
dasprasky <prasenjitdaswk@gmail.com>
2024-11-28 18:41:53 +0800

    add vehicle data to ui in mcu summary

--- app/src/main/java/com/vismo/nextgenmeter/ui/dashboard/mcu/MCUSummaryDashboardViewModel.kt
+++ app/src/main/java/com/vismo/nextgenmeter/ui/dashboard/mcu/MCUSummaryDashboardViewModel.kt
@@ -1,13 +1,19 @@
 package com.vismo.nextgenmeter.ui.dashboard.mcu
 
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ilin.util.ShellUtils
 import com.vismo.nextgenmeter.datastore.DeviceDataStore
 import com.vismo.nextgenmeter.datastore.DeviceDataStore.mcuPriceParams
+import com.vismo.nextgenmeter.model.OperatingArea
 import com.vismo.nextgenmeter.module.IoDispatcher
 import com.vismo.nextgenmeter.repository.MeasureBoardRepository
 import com.vismo.nextgenmeter.repository.MeterPreferenceRepository
+import com.vismo.nextgenmeter.repository.RemoteMeterControlRepository
+import com.vismo.nextgenmeter.ui.theme.lantauBlue
+import com.vismo.nextgenmeter.ui.theme.ntGreen
+import com.vismo.nextgenmeter.ui.theme.urbanRed
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -23,7 +29,8 @@ import javax.inject.Inject
 class MCUSummaryDashboardViewModel @Inject constructor(
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
     private val measureBoardRepository: MeasureBoardRepository,
-    private val meterPreferenceRepository: MeterPreferenceRepository
+    private val meterPreferenceRepository: MeterPreferenceRepository,
+    private val remoteMeterControlRepository: RemoteMeterControlRepository
 ) : ViewModel() {
 
     private val _mcuSummaryUiState = MutableStateFlow(MCUSummaryUiData())
@@ -65,6 +72,36 @@ class MCUSummaryDashboardViewModel @Inject constructor(
                     }
                 }
             }
+            launch {
+                remoteMeterControlRepository.meterInfo.collectLatest {
+                    it?.let {
+                        mutex.withLock {
+                            _mcuSummaryUiState.value = _mcuSummaryUiState.value.copy(
+                                vehicle = it.settings?.vehicle,
+                                operatingArea = getOperatingAreaZH(it.settings?.operatingArea),
+                                operatingAreaColor = getOperatingAreaColor(it.settings?.operatingArea ?: OperatingArea.URBAN),
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getOperatingAreaZH(operatingArea: OperatingArea?): String {
+        return when(operatingArea) {
+            OperatingArea.LANTAU -> "大嶼山"
+            OperatingArea.NT -> "新界"
+            OperatingArea.URBAN -> "市區"
+            else -> ""
+        }
+    }
+
+    private fun getOperatingAreaColor(operatingArea: OperatingArea): Color {
+        return  when(operatingArea) {
+            OperatingArea.LANTAU -> lantauBlue
+            OperatingArea.NT -> ntGreen
+            OperatingArea.URBAN -> urbanRed
         }
     }
 
