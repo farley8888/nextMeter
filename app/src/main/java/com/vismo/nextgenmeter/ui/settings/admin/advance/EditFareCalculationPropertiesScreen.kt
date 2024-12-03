12e2e9d0084b6bcccd41026d94a516c69b33f392
dasprasky <prasenjitdaswk@gmail.com>
2024-12-03 15:58:00 +0800

    Dash 2212, 2210 - update fare screen and data handling

--- app/src/main/java/com/vismo/nextgenmeter/ui/settings/admin/advance/EditFareCalculationPropertiesScreen.kt
+++ app/src/main/java/com/vismo/nextgenmeter/ui/settings/admin/advance/EditFareCalculationPropertiesScreen.kt
@@ -27,7 +27,7 @@ import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.vismo.nextgenmeter.model.format
 import com.vismo.nextgenmeter.ui.settings.admin.EditAdminPropertiesViewModel
 import com.vismo.nextgenmeter.ui.shared.GlobalSnackbarDelegate
 import com.vismo.nextgenmeter.ui.shared.SnackbarState
@@ -41,15 +41,15 @@ fun EditFareCalculationPropertiesScreen(
     viewModel: EditAdminPropertiesViewModel,
     snackbarDelegate: GlobalSnackbarDelegate
 ) {
-    val mcuPriceParams = viewModel.mcuPriceParams.collectAsState()
+    val mcuPriceParams = viewModel.mcuPriceParams.collectAsState().value?.format()
     var priceParam1Entered: String?  by remember { mutableStateOf(null) }
     var priceParam2Entered: String?  by remember { mutableStateOf(null) }
     var priceParam3Entered: String?  by remember { mutableStateOf(null) }
     var priceParam4Entered: String?  by remember { mutableStateOf(null) }
-    val startPriceStr = mcuPriceParams.value?.startingPrice?.replace("$", "")?.takeIf { priceParam1Entered == null } ?: priceParam1Entered!!
-    val stepPriceStr = mcuPriceParams.value?.stepPrice?.replace("$", "")?.takeIf { priceParam2Entered == null } ?: priceParam2Entered!!
-    val changedStepPriceStr = mcuPriceParams.value?.changedStepPrice?.replace("$", "")?.takeIf { priceParam3Entered == null } ?: priceParam3Entered!!
-    val changedPriceAtStr = mcuPriceParams.value?.changedPriceAt?.replace("$", "")?.takeIf { priceParam4Entered == null } ?: priceParam4Entered!!
+    val startPriceStr = mcuPriceParams?.startingPrice?.takeIf { priceParam1Entered == null } ?: priceParam1Entered!!
+    val changePriceAtStr = mcuPriceParams?.changedPriceAt?.takeIf { priceParam2Entered == null } ?: priceParam2Entered!!
+    val stepPriceStr = mcuPriceParams?.stepPrice?.takeIf { priceParam3Entered == null } ?: priceParam3Entered!!
+    val changedStepPriceStr = mcuPriceParams?.changedStepPrice?.takeIf { priceParam4Entered == null } ?: priceParam4Entered!!
 
     Column(
         modifier = Modifier
@@ -72,7 +72,7 @@ fun EditFareCalculationPropertiesScreen(
             Spacer(modifier = Modifier.width(16.dp))
             Text(text = "分水嶺", modifier = Modifier.weight(1f))
             TextField(
-                value = stepPriceStr,
+                value = changePriceAtStr,
                 onValueChange = { newText -> priceParam2Entered = newText },
                 modifier = Modifier.weight(3f),
                 keyboardOptions = KeyboardOptions(
@@ -84,7 +84,7 @@ fun EditFareCalculationPropertiesScreen(
         Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
             Text(text = "每200米", modifier = Modifier.weight(1f))
             TextField(
-                value = changedStepPriceStr,
+                value = stepPriceStr,
                 onValueChange = { newText -> priceParam3Entered = newText },
                 modifier = Modifier.weight(3f),
                 keyboardOptions = KeyboardOptions(
@@ -94,7 +94,7 @@ fun EditFareCalculationPropertiesScreen(
             Spacer(modifier = Modifier.width(16.dp))
             Text(text = "後200米", modifier = Modifier.weight(1f))
             TextField(
-                value = changedPriceAtStr,
+                value = changedStepPriceStr,
                 onValueChange = { newText -> priceParam4Entered = newText },
                 modifier = Modifier.weight(3f),
                 keyboardOptions = KeyboardOptions(
@@ -107,17 +107,17 @@ fun EditFareCalculationPropertiesScreen(
             onClick = {
                 val startPrice = (startPriceStr.toDoubleOrNull()?.times(100))?.toInt()
                 val stepPrice = (stepPriceStr.toDoubleOrNull()?.times(5)?.times(100))?.toInt()
-                val stepPrice2nd = (changedStepPriceStr.toDoubleOrNull()?.times(5)?.times(100))?.toInt()
-                val threshold = (changedPriceAtStr.toDoubleOrNull()?.times(10))?.toInt()
+                val stepPrice2 = (changedStepPriceStr.toDoubleOrNull()?.times(5)?.times(100))?.toInt()
+                val threshold = (changePriceAtStr.toDoubleOrNull()?.times(10))?.toInt()
 
-                if (startPrice == null || stepPrice == null || stepPrice2nd == null || threshold == null) {
+                if (startPrice == null || stepPrice == null || stepPrice2 == null || threshold == null) {
                     snackbarDelegate.showSnackbar(SnackbarState.ERROR, "Invalid values entered.")
                     return@Button
                 }
                 viewModel.updatePriceParams(
                     startPrice = startPrice,
                     stepPrice = stepPrice,
-                    stepPrice2nd = stepPrice2nd,
+                    stepPrice2nd = stepPrice2,
                     threshold = threshold
                 )
                 snackbarDelegate.showSnackbar(SnackbarState.SUCCESS,"Fare parameters updated")
