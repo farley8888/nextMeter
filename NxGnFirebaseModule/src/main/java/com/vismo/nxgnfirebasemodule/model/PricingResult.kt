63d94028114ac15fa168b569a230b7673c8dd327
dasprasky <prasenjitdaswk@gmail.com>
2024-12-05 18:58:33 +0800

    temporarily remove discount

--- NxGnFirebaseModule/src/main/java/com/vismo/nxgnfirebasemodule/model/PricingResult.kt
+++ NxGnFirebaseModule/src/main/java/com/vismo/nxgnfirebasemodule/model/PricingResult.kt
@@ -2,6 +2,6 @@ package com.vismo.nxgnfirebasemodule.model
 
 data class PricingResult(
     val applicableFee: Double,
-    val applicableDiscount: Double,
+    val applicableDiscount: Double? = null,
     val applicableTotal: Double,
 )
