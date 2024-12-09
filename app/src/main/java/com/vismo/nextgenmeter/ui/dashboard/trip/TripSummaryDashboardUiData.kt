41fb34941010220128c4f1dc0e578fcaacfb35d6
dasprasky <prasenjitdaswk@gmail.com>
2024-12-09 14:54:41 +0800

    reset trip dashboard when trips are cleared

--- app/src/main/java/com/vismo/nextgenmeter/ui/dashboard/trip/TripSummaryDashboardUiData.kt
+++ app/src/main/java/com/vismo/nextgenmeter/ui/dashboard/trip/TripSummaryDashboardUiData.kt
@@ -3,7 +3,7 @@ package com.vismo.nextgenmeter.ui.dashboard.trip
 data class TripSummaryDashboardUiData (
     val type: TripSummaryDashboardType,
     val totalTrips: String = "0",
-    val totalWaitTime: String = "0",
+    val totalWaitTime: String = "00:00:00",
     val totalDistanceInKM: String = "0",
     val totalFare: String = "$0.0",
     val totalExtras: String = "$0.0",
