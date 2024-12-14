841e5618057ff7eb35d3517868ec33ba48e6a835
dasprasky <prasenjitdaswk@gmail.com>
2024-12-14 18:11:55 +0800

    DASH-2265 - show license plate and meter serial number on pairing screen

--- app/src/main/java/com/vismo/nextgenmeter/ui/meter/MeterOpsScreen.kt
+++ app/src/main/java/com/vismo/nextgenmeter/ui/meter/MeterOpsScreen.kt
@@ -47,10 +47,8 @@ import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
